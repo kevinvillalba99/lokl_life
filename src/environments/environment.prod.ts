b241b5b6d16d00aff870ecb5e6563c9977423a99
Estiven Mahecha henao <mahechahe@gmail.com>
2023-07-31 20:18:48 -0500

    [ADD] Add envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: true,
   url: 'https://develop.api.lokl.life/api/v1/',
   unit_value: 112000,
+  owner_wompi: '646fcef8c158685da367ec02',
+  project_wompi: '63261a94c8011a8a836fda23',
+  redirect_url_wompi: 'https://lokl.life/payment/successful',
+  token_sandbox: 'pub_test_srHBcxqYISn8FsYZLuSWeOmhU679yCV5'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,10 @@ export const environment = {
   production: false,
   url: 'https://develop.api.lokl.life/api/v1/',
   unit_value: 112000,
+  owner_wompi: '646fcef8c158685da367ec02',
+  project_wompi: '63261a94c8011a8a836fda23',
+  redirect_url_wompi: 'https://lokl.life/payment/successful',
+  token_sandbox: 'pub_test_srHBcxqYISn8FsYZLuSWeOmhU679yCV5'
 };
 
 /*
